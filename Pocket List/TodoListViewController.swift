e6d7e53dfe6e1432cb3d56389b4e7d0a2febd617
quantacake <teddiepenbrock@gmail.com>
2019-08-16 23:29:33 -0500

    Todo list complete with datasource and delegate methods.

---
+++ Pocket List/TodoListViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Pocket List
+//
+//  Created by ted diepenbrock on 8/16/19.
+//  Copyright © 2019 ted diepenbrock. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // reusable cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row] // current row of current index path
+        
+        return cell
+    }
+    
+    // MARK - TableView Delegate Methods
+    // prints row when user selects a cell
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(itemArray[indexPath.row])
+        
+        // add or remove checkmark accessory to cell when on tap
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+//        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+
+
+}
+
